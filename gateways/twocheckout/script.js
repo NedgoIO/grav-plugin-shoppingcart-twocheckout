dbe0f2dde6a3126e356625fa37ad11045e29ead7
Mitchell Ogden <mitch@levlup.co>
2017-05-10 16:02:49 -0400

    change to twocheckout

--- gateways/twocheckout/script.js
+++ gateways/twocheckout/script.js
@@ -6,22 +6,22 @@
     jQuery(function() {
         jQuery(document).on('proceedToPayment', function(event, ShoppingCart) {
 
-            if (ShoppingCart.gateway != 'stripe') {
+            if (ShoppingCart.gateway != 'twocheckout') {
                 return;
             }
 
             var amount = ShoppingCart.calculateTotalPriceIncludingTaxesAndShipping();
 
-            var stripeHandler = StripeCheckout.configure({
-                key: ShoppingCart.settings.payment.methods.stripe.publicKey,
+            var twocheckoutHandler = TwocheckoutCheckout.configure({
+                key: ShoppingCart.settings.payment.methods.twocheckout.publicKey,
                 token: function(token, args) {
                     var order = {
                         products: storejs.get('grav-shoppingcart-basket-data'),
                         data: storejs.get('grav-shoppingcart-checkout-form-data'),
                         shipping: storejs.get('grav-shoppingcart-shipping-method'),
-                        payment: 'stripe',
+                        payment: 'twocheckout',
                         token: storejs.get('grav-shoppingcart-order-token').token,
-                        extra: { 'stripeToken': token.id },
+                        extra: { 'twocheckoutToken': token.id },
                         taxes: ShoppingCart.taxesApplied.toString(),
                         amount: amount,
                         gateway: ShoppingCart.gateway
@@ -42,11 +42,11 @@
                 }
             });
 
-            stripeHandler.open({
-                name: ShoppingCart.settings.payment.methods.stripe.name,
-                description: ShoppingCart.settings.payment.methods.stripe.description,
+            twocheckoutHandler.open({
+                name: ShoppingCart.settings.payment.methods.twocheckout.name,
+                description: ShoppingCart.settings.payment.methods.twocheckout.description,
                 email: storejs.get('grav-shoppingcart-checkout-form-data').email,
-                amount: parseInt(amount.toString().replace('.', '')), //use cents, as required by Stripe
+                amount: parseInt(amount.toString().replace('.', '')), //use cents, as required by Twocheckout
                 currency: ShoppingCart.settings.general.currency
             });
         });
